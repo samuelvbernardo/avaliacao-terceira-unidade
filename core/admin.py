d1d717aa229cdfc70e844d4f260841199f5b7354
Samuel Bernardo <samuelbenardo@ads.fiponline.edu.br>
2025-05-29 16:23:44 -0300

    chore: register models in Django admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Paciente, Medico, Consulta
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import Paciente, Medico, Consulta, Perfil
 
 # Registrando os modelos no Django Admin
 admin.site.register(Paciente)
 admin.site.register(Medico)
 admin.site.register(Consulta)
+
+class PerfilInline(admin.StackedInline):
+    model = Perfil
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [PerfilInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
